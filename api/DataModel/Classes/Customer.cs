58d0156b1f9636feb78208fd14fda035c30c76cd
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2018-08-11 23:21:41 -0700

    initial api

---
+++ api/DataModel/Classes/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DataModel.Classes
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int VehicleId { get; set; }
+    }
+}
---
+++ api/DataModel/Classes/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DataModel.Classes
+{
+    public enum Color 
+    {
+        White,
+        Black,
+        Silver,
+        Green
+    }
+
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int Miles { get; set; }
+        public Color Color { get; set; }
+        public string Model { get; set; }
+    }
+}
